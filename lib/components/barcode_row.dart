b799af1e132617463779f55290a25892c9e91f49
Jery <jery99961@gmail.com>
2024-08-20 01:48:50 +0530

    Display barcode rows within a Card widget

--- lib/components/barcode_row.dart
+++ lib/components/barcode_row.dart
@@ -14,42 +14,44 @@ class BarcodeRow extends StatelessWidget {
             ? "Not yet Scanned"
             : document['timestamp'].toDate().toString();
     
-    return ListTile(
-      title: Text(document.id),
-      subtitle: Text(subtitle),
-      trailing: CircleAvatar(
-        backgroundColor: document['scanned'] ? Colors.green : Colors.blueGrey,
-        child: Icon(
-          document['scanned'] ? Icons.check : Icons.pending,
-          color: Colors.white,
+    return Card(
+      child: ListTile(
+        title: Text(document.id),
+        subtitle: Text(subtitle),
+        trailing: CircleAvatar(
+          backgroundColor: document['scanned'] ? Colors.green : Colors.blueGrey,
+          child: Icon(
+            document['scanned'] ? Icons.check : Icons.pending,
+            color: Colors.white,
+          ),
         ),
+        onLongPress: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Confirm Reset"),
+                content: const Text("Are you sure you want to reset this barcode?"),
+                actions: [
+                  TextButton(
+                    child: const Text("Cancel"),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
+                  TextButton(
+                    child: const Text("Confirm"),
+                    onPressed: () {
+                      Database.resetBarcode(document.id);
+                      Navigator.of(context).pop(true);
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
-      onLongPress: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text("Confirm Reset"),
-              content: const Text("Are you sure you want to reset this barcode?"),
-              actions: [
-                TextButton(
-                  child: const Text("Cancel"),
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                ),
-                TextButton(
-                  child: const Text("Confirm"),
-                  onPressed: () {
-                    Database.resetBarcode(document.id);
-                    Navigator.of(context).pop(true);
-                  },
-                ),
-              ],
-            );
-          },
-        );
-      },
     );
   }
 }
