3e37e0d73f2614eee554d811326fb562ae0a1c79
Jery <jery99961@gmail.com>
2025-08-10 11:10:09 +0530

    feat: enhance build workflows to support iOS and Android APKs with configurable inputs

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,14 @@ import 'services/collection_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  print('Initializing Firebase...');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
-  // Load saved collection
+  print('Firebase initialized. Loading saved collection...');
   await CollectionManager.loadSavedCollection();
-  
+  print('Loaded collection: '
+      '${CollectionManager.currentCollection}');
   runApp(const MyApp());
 }
 
@@ -23,6 +24,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('Building MyApp. currentCollection: '
+        '${CollectionManager.currentCollection}');
     return MaterialApp(
       title: 'Event Scan',
       theme: ThemeData(
@@ -31,8 +34,27 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: CollectionManager.currentCollection != null 
-          ? const HomeScreen() 
-          : const CollectionSelectionScreen(),
+          ? const DebugHomeScreen() 
+          : const DebugCollectionSelectionScreen(),
     );
+  }
+}
+
+// Debug wrappers to print when screens are built
+class DebugHomeScreen extends StatelessWidget {
+  const DebugHomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    print('Showing HomeScreen');
+    return const HomeScreen();
+  }
+}
+
+class DebugCollectionSelectionScreen extends StatelessWidget {
+  const DebugCollectionSelectionScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    print('Showing CollectionSelectionScreen');
+    return const CollectionSelectionScreen();
   }
 }
